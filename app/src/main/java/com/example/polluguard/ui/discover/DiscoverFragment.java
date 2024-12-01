129a912c613f908a70dd63076e91e94c92c85186
JesselynW <146332547+JesselynW@users.noreply.github.com>
2024-12-01 16:18:15 +0700

    template background + color palate + volunteer page ui

--- app/src/main/java/com/example/polluguard/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/polluguard/ui/discover/DiscoverFragment.java
@@ -24,8 +24,8 @@ public class DiscoverFragment extends Fragment {
         binding = FragmentDiscoverBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        discoverViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textNotifications;
+//        discoverViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
