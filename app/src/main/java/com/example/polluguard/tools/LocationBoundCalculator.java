882398ffee3c432ac2b47b5bd6300d5b7141abe0
ellena <86466041+ellenaitsme@users.noreply.github.com>
2024-12-23 00:02:24 +0700

    Nearest Location AQI + Other Home Page Elements

---
+++ app/src/main/java/com/example/polluguard/tools/LocationBoundCalculator.java
@@ -0,0 +1,19 @@
+package com.example.polluguard.tools;
+
+public class LocationBoundCalculator {
+    public static double[] calculateLocationBound(double lat, double lon, double radius) {
+        double[] result;
+
+       double deltaLat = radius / 110.574; // approximately 1 deg lat = 110.574
+        double deltaLon = radius / (111.32 * Math.cos(Math.toRadians(lat))); // approximately 1 deg lng = 111.32 * cos(latitude)
+
+        double minLat = lat - deltaLat;
+        double maxLat = lat + deltaLat;
+        double minLon = lon - deltaLon;
+        double maxLon = lon + deltaLon;
+
+        result = new double[]{minLat, minLon, maxLat, maxLon};
+
+        return result;
+    }
+}
